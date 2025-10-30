1117428453a4d7c6e7e854836519f3ea223c4f44
roshan shrishrimal <roshanshrishrimal4@gmail.com>
2025-10-30 10:57:35 +0530

    done file

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os 
 
 print(os.directory)
-<<<<<<< HEAD
-print(os.print())
-print(os.it is () and ())
-=======
-print(os.print)
->>>>>>> 82c7e4e40c750ce09abb5ca22d3f7daf97817015
+print(object)
+print(sum)
+print(super)
